67bf3fb0f6955fed5eaf0f25d4dedbec39d8962e
Vinícius Yoda <vyoda4604@gmail.com>
2025-06-24 19:21:38 -0300

    id

--- src/app.js
+++ src/app.js
@@ -10,6 +10,14 @@ const selecoes = [
     {id: 4, selecao: 'Camarões', grupo: 'G'},
 ]
 
+function buscarSelecaoPorId(id) {
+    return selecoes.filter(selecao => selecao.id == id)
+}
+
+function buscarIndexSelecao(id) {
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
+
 app.get('/', (req, res) => {
     res.send('Olá mundo')
 })
@@ -18,9 +26,26 @@ app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
 })
 
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/selecoes', (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com sucesso!')
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send('Seleção excluida com sucesso')
+})
+
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo = req.body.grupo
+    res.json(selecoes)
 })
 
 export default app
