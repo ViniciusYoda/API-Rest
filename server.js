ed6b68065f80e8e7d83a6412989a0940209527d5
Vinícius Yoda <vyoda4604@gmail.com>
2025-06-29 09:44:28 -0300

    fim

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './src/app.js'
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereõ http://localhost:${PORT}`)
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
 })
--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 import express from 'express'
-import SelecaoController from './app/controllers/SelecaoController.js'
+import router from './routes'
 const app = express()
 
 app.use(express.json())
-
-app.get('/selecoes', SelecaoController.index)
-app.get('/selecoes/:id', SelecaoController.show)
-app.post('/selecoes', SelecaoController.store)
-app.delete('/selecoes/:id', SelecaoController.delete)
-app.put('/selecoes/:id', SelecaoController.update)
+app.use(router)
 
 export default app
